ab8c2b238230c5209ac9d47cf038f7503b1edf88
NoedL <88205753+N0edL@users.noreply.github.com>
2025-03-25 13:56:18 +0100

    Added Loading of savefiles

--- lib/manager.py
+++ lib/manager.py
@@ -1,6 +1,10 @@
-class Manager:
-    def __init__(self):
-        self.data = {}
 
-    def add(self, key, value):
-        self.data[key] = value
+from pathlib import Path
+
+savefile_dir = next((x for x in (Path.home() / "AppData" / "LocalLow" / "TVGS" / "Schedule I" / "saves").iterdir() if x.is_dir()), None)
+
+def load_save_folders():
+    if savefile_dir is None:
+        return
+    for x in savefile_dir.iterdir():
+        return {"name": x.name, "path": x}
